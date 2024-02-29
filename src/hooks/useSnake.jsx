32e384c26b7b32326df3e2172da7bd6ac4d973d1
Jus Lozej <jus.lozej@sealtime.fish>
2024-02-29 12:30:01 +0100

    Prevent inputs from a moving backwards into oneself

--- src/hooks/useSnake.jsx
+++ src/hooks/useSnake.jsx
@@ -3,28 +3,29 @@ import { useEffect, useState } from "react";
 const useSnake = (initialDirection = 6) => {
   const [snakeDirection, setSnakeDirection] = useState(initialDirection);
 
-  const handleKeyDown = (e) => {
-    if (e.keyCode == 37) {
-      // Left
-      setSnakeDirection(4);
-    } else if (e.keyCode == 38) {
-      // up
-      setSnakeDirection(8);
-    } else if (e.keyCode == 39) {
-      // right
-      setSnakeDirection(6);
-    } else if (e.keyCode == 40) {
-      // down
-      setSnakeDirection(2);
-    }
-  };
-
   useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.keyCode == 37 && snakeDirection != 6) {
+        // Left
+        setSnakeDirection(4);
+      } else if (e.keyCode == 38 && snakeDirection != 2) {
+        // up
+        setSnakeDirection(8);
+      } else if (e.keyCode == 39 && snakeDirection != 4) {
+        // right
+        setSnakeDirection(6);
+      } else if (e.keyCode == 40 && snakeDirection != 8) {
+        // down
+        setSnakeDirection(2);
+      }
+    };
+
     document.addEventListener("keydown", handleKeyDown);
     return () => {
-      document.addEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keydown", handleKeyDown);
     };
   }, [snakeDirection]);
+
   return snakeDirection;
 };
 export default useSnake;
